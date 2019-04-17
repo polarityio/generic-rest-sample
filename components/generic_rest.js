c1bd78f91e9d940879b533c584269f579a00e4ba
Ed <ed@polarity.io>
2019-04-17 11:48:04 -0400

    Add onDetails and onMessage to sample

--- components/generic_rest.js
+++ components/generic_rest.js
@@ -1,6 +1,27 @@
 'use strict';
 
 polarity.export = PolarityComponent.extend({
+    message: '',
+    actions: {
+        // this action is triggered by the button in our template which calls the `incrementCounter` action
+        incrementCounter: function() {
+            // The payload should be a javascript object literal but can contain any properties
+            let payload = {
+                type: 'COUNT_CLICKS',
+                data: {
+                    title: 'You can pass '
+                }
+            };
 
+            this.sendIntegrationMessage(payload)
+                .then((response) => {
+                    // set our `message` property to the value of the response.message property sent from the server
+                    this.set('message', response.message);
+                })
+                .catch((err) => {
+                    // Catch any errors and print out the error after converting it to a string
+                    this.set('message', `Error: ${JSON.stringify(err)}`);
+                });
+        }
+    }
 });
-
--- integration.js
+++ integration.js
@@ -13,7 +13,8 @@ const BASE_URI = 'http://httpbin.org/get';
  * @param logger logger object
  */
 function startup(logger) {
-    Logger = logger;
+  // cache a reference to the logger
+  Logger = logger;
 }
 
 /**
@@ -22,76 +23,114 @@ function startup(logger) {
  * @param cb callback function
  */
 function doLookup(entities, options, cb) {
-    let lookupResults = [];
+  let lookupResults = [];
 
-    async.each(entities, function (entityObj, next) {
-        _lookupEntity(entityObj, options, function (err, result) {
-            if (err) {
-                next(err);
-            } else {
-                lookupResults.push(result);
-                next(null);
-            }
-        });
-    }, function (err) {
-        cb(err, lookupResults);
-    });
+  async.each(
+    entities,
+    function(entityObj, next) {
+      _lookupEntity(entityObj, options, function(err, result) {
+        if (err) {
+          next(err);
+        } else {
+          lookupResults.push(result);
+          next(null);
+        }
+      });
+    },
+    function(err) {
+      cb(err, lookupResults);
+    }
+  );
 }
 
 function _lookupEntity(entityObj, options, cb) {
-    let uri = BASE_URI;
+  let uri = BASE_URI;
 
-    if (options.apiKey.length > 0) {
-        uri += '?apiKey=' + options.apiKey;
-    }
+  if (options.apiKey.length > 0) {
+    uri += '?apiKey=' + options.apiKey;
+  }
 
-    request({
-        uri: uri,
-        method: 'GET',
-        json: true
-    }, function (err, response, body) {
-        // check for a request error
-        if (err) {
-            cb({
-                detail: 'Error Making HTTP Request',
-                debug: err
-            });
-            return;
-        }
-
-        // If we get a 404 then cache a miss
-        if(response.statusCode === 404){
-            cb(null, {
-                entity: entityObj,
-                data: null  // setting data to null indicates to the server that this entity lookup was a "miss"
-            });
-            return;
-        }
-
-        if (response.statusCode !== 200) {
-            cb({
-                detail: 'Unexpected HTTP Status Code Received',
-                debug: body
-            });
-            return;
-        }
-
-        // The lookup results returned is an array of lookup objects with the following format
-        cb(null, {
-            // Required: This is the entity object passed into the integration doLookup method
-            entity: entityObj,
-            // Required: An object containing everything you want passed to the template
-            data: {
-                // Required: These are the tags that are displayed in your template
-                summary: [body.origin],
-                // Data that you want to pass back to the notification window details block
-                details: body
-            }
+  request(
+    {
+      uri: uri,
+      method: 'GET',
+      json: true
+    },
+    function(err, response, body) {
+      // check for a request error
+      if (err) {
+        return cb({
+          detail: 'Error Making HTTP Request',
+          err: err
         });
-    });
+      }
+
+      // If we get a 404 then cache a miss
+      if (response.statusCode === 404) {
+        return cb(null, {
+          entity: entityObj,
+          data: null // setting data to null indicates to the server that this entity lookup was a "miss"
+        });
+      }
+
+      if (response.statusCode !== 200) {
+        cb({
+          detail: `Unexpected HTTP Status Code Received ${response.statusCode}`,
+          debug: body
+        });
+        return;
+      }
+
+      // The lookup results returned is an array of lookup objects with the following format
+      cb(null, {
+        // Required: This is the entity object passed into the integration doLookup method
+        entity: entityObj,
+        // Required: An object containing everything you want passed to the template
+        data: {
+          // Required: These are the tags that are displayed in your template
+          summary: [body.origin],
+          // Data that you want to pass back to the notification window details block
+          details: body
+        }
+      });
+    }
+  );
+}
+
+function onDetails(lookupObject, options, cb) {
+  // We're adding an artificial 3 second delay before we respond back to the onDetails request
+  setTimeout(function() {
+    // Add some new information to our details
+    lookupObject.data.details.newOnDetailsData = {
+      key: 'This is some onDetails data'
+    };
+
+    // Add a new tag
+    lookupObject.data.summary.push('onDetails Tag');
+
+    // Send back just the `data` property on the lookupObject
+    cb(null, lookupObject.data);
+  }, 3000);
+}
+
+let messageCounter = 0;
+function onMessage(payload, options, cb) {
+  switch (payload.type) {
+    case 'COUNT_CLICKS':
+      cb(null, {
+        message: `I've been clicked ${++messageCounter} times`
+      });
+      break;
+    default:
+      cb({
+        detail: `Unexpected onMessage type received: ${payload.type}`
+      });
+  }
 }
 
 module.exports = {
-    doLookup: doLookup,
-    startup: startup
+  doLookup: doLookup,
+  startup: startup,
+  onDetails: onDetails,
+  onMessage: onMessage
 };
